937725f2259bdc4ef36a80d38b850f2e92a4f459
William.Wu <william30101@gmail.com>
2017-08-27 01:43:24 +0800

    Add firebase , user login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, Renderer, OnDestroy } from '@angular/core';
+import {FirebaseApiService} from '../../shared/firebase-api.service';
+import {UserService} from '../../shared/user.service';
+import {NgForm} from '@angular/forms';
+import * as firebase from 'firebase';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,16 +11,60 @@ import { Component, OnInit, Renderer, OnDestroy } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
+  emailVerificationMessage: boolean = false;
 
-  constructor(private renderer: Renderer) {
+  constructor(private renderer: Renderer,
+              private fire: FirebaseApiService,
+              private user: UserService,
+              private router: Router) {
     this.renderer.setElementClass(document.body, 'login', true);
   }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => {
+        if (!user.emailVerified) {
+          this.emailVerificationMessage = true;
+          this.logout();
+          return null;
+        } else {
+          return this.fire.getUserFromDatabase(user.uid);
+        }
+
+      })
+      .then(userFromDatabase => {
+        if (userFromDatabase) {
+          this.user.set(userFromDatabase);
+          this.router.navigate(['/dashboard']);
+        }
+
+      })
+      .catch(err => {
+       // this.notifier.display('error', err);
+        console.log('error' + err);
+      });
+
+  }
+
+
   ngOnDestroy() {
     this.renderer.setElementClass(document.body, 'login', false);
   }
+
+
+  logout() {
+    firebase.auth().signOut()
+      .catch(err => {
+        // this.notifier.display('error', err);
+        console.log('signout err ' + err);
+      });
+  }
+
 
 }
---
+++ src/app/shared/user.service.ts
@@ -0,0 +1,59 @@
+import {EventEmitter, Injectable} from '@angular/core';
+import * as firebase from 'firebase';
+
+@Injectable()
+export class UserService {
+
+
+  statusChange = new EventEmitter<any>();
+
+  // constructor(private liveMessages: LiveMessageService) { }
+
+  constructor() { }
+
+  set(userFromDatabase) {
+    localStorage.setItem('user', JSON.stringify(userFromDatabase));
+    this.statusChange.emit(userFromDatabase);
+
+    const messaging = firebase.messaging();
+
+    messaging.requestPermission()
+      .then(() => {
+
+        firebase.messaging().getToken()
+          .then(token => {
+            console.log('Token received: ', token);
+            const updates = {};
+
+            messaging.onMessage(payload => {
+              console.log(payload);
+              // this.liveMessages.display((payload as any).notification);
+            });
+
+            updates['/users/' + userFromDatabase.uid + '/messageToken'] = token;
+            return firebase.database().ref().update(updates);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }
+
+  destroy() {
+    localStorage.removeItem('user');
+    this.statusChange.emit(null);
+    console.log('user removed from localstorage');
+  }
+
+  getProfile() {
+    const user = localStorage.getItem('user');
+    return JSON.parse(user);
+  }
+
+
+}
