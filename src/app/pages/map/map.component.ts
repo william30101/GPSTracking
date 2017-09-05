b3908636f89f274c5f218ed6311ad9014624c7a8
William.Wu <william30101@gmail.com>
2017-09-05 21:28:26 +0800

    Add map button function

--- src/app/pages/map/map.component.ts
+++ src/app/pages/map/map.component.ts
@@ -10,15 +10,78 @@ export class MapComponent implements OnInit {
   lat: number = 51.678418;
   lng: number = 7.809007;
   radi: number = 1000;
-
+  zoom: number = 10;
   colo: String = 'DarkRed';
 
-  lat2: number = 51.678418;
-  lng2: number = 8.809007;
+
+  markers: marker[];
+
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onCurrentLocationBtn() {
+    console.log('onCurrentLocationBtn be clicked');
+    this.lat = 25.049887;
+    this.lng = 121.575095;
+
+    this.zoom = 15;
+
+  }
+
+  onHistoryBtn() {
+    console.log('onHistoryBtn be clicked');
+
+    this.markers = [
+    {
+      lat: 25.048013,
+        lng: 121.577084,
+      label: 'A',
+      draggable: true
+    },
+    {
+      lat: 25.046545,
+        lng: 121.573865,
+      label: 'B',
+      draggable: false
+    },
+    {
+      lat: 25.046117,
+        lng: 121.570174,
+      label: 'C',
+      draggable: true
+    },
+    {
+      lat: 25.046117,
+        lng: 121.560174,
+      label: 'D',
+      draggable: true
+    },
+    {
+      lat: 25.046117,
+        lng: 121.550174,
+      label: 'E',
+      draggable: true
+    },
+    {
+      lat: 25.046117,
+        lng: 121.540174,
+      label: 'F',
+      draggable: true
+    }
+    ];
+
+  }
+
 }
+
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
+
