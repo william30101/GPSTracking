649f233f90ff1b34770afcfdcc51e26cd6228f15
William.Wu <william30101@gmail.com>
2017-09-10 23:02:04 +0800

    Modify UI

--- src/app/app.conf.ts
+++ src/app/app.conf.ts
@@ -1,12 +1,12 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCii4JCVPbozCJq3oSxDYGUlGKmqLhLFME',
-  authDomain: 'gpstracking-7df75.firebaseapp.com',
-  databaseURL: 'https://gpstracking-7df75.firebaseio.com',
-  projectId: 'gpstracking-7df75',
-  storageBucket: 'gpstracking-7df75.appspot.com',
-  messagingSenderId: '335491343092'
+  apiKey: '********',
+  authDomain: '********',
+  databaseURL: '********',
+  projectId: '********',
+  storageBucket: '********',
+  messagingSenderId: '********'
 };
 
 export const agmConfig = {
-  apiKey: 'AIzaSyCsuIt8ZPToELVz_r5gOJJIACfMDbJ7ZHA'
+  apiKey: '********'
 };
