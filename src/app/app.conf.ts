d4753a622d251c4e919d85351d44e8f6efc58f78
William.Wu <william30101@gmail.com>
2017-09-18 22:22:26 +0800

    Modify code

--- src/app/app.conf.ts
+++ src/app/app.conf.ts
@@ -1,12 +1,12 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDkYdzU-h3Nrku4SzSnrwYoCDSnPi9luZw',
-  authDomain: 'gpstracking-177915.firebaseapp.com',
-  databaseURL: 'https://gpstracking-177915.firebaseio.com',
-  projectId: 'gpstracking-177915',
-  storageBucket: 'gpstracking-177915.appspot.com',
-  messagingSenderId: '27601170257'
+  apiKey: '********',
+  authDomain: '********',
+  databaseURL: '********',
+  projectId: '********',
+  storageBucket: '********',
+  messagingSenderId: '********'
 };
 
 export const agmConfig = {
-  apiKey: 'AIzaSyCsuIt8ZPToELVz_r5gOJJIACfMDbJ7ZHA'
+  apiKey: '********'
 };
